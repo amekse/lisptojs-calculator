ece37b83f48591af2fe5fd459d7621425965b617
Saswata Das <dassaswata1996@gmail.com>
2023-05-10 18:15:21 +0530

    fix defun check

--- src/syntax-analysis.js
+++ src/syntax-analysis.js
@@ -2,31 +2,39 @@
     Developing the command execution tree
 */
 
-import { logError, logOutput } from './utils/logger.js'
+import { logDebug, logError, logOutput } from './utils/logger.js'
 
 const inKeywordsLChecklist = (keyword) => ["defun", "write", "terpri"].includes(keyword);
 
-const checkLFunctionName = (funcName, functionsLList) => (typeof funcName !== "string" || !isNaN(funcName) || inKeywordsLChecklist(funcName) || functionsLList.includes(funcName)) ?
-    logError("Syntax Error", `${funcName} cannot be a function name`) : null;
+const checkLDefun = (funcName, functionsLList) => (typeof funcName !== "string" || !isNaN(funcName) || inKeywordsLChecklist(funcName) || functionsLList.includes(funcName)) ?
+    {action: null, value: null, error: `${funcName} cannot be a function name`} : {action: "defun", value: funcName, error: null};
 
 const checkSyntaxStructure = (commandList, index, functionsLList) => {
+    let lastFuncLList = functionsLList;
     if (index >= 0) {
+        let checkRes = {action: null, value: null, error: null};
         if (typeof commandList[index] === "object") {
-            checkSyntaxStructure(commandList[index], commandList[index].length-1, functionsLList);
+            checkSyntaxStructure(commandList[index], commandList[index].length-1, lastFuncLList);
         } else {
             if (commandList[index] === "defun") {
-                checkLFunctionName(commandList[index+1], functionsLList);
-            }
-            if (commandList[index] === "write") {
-                
+                checkRes = checkLDefun(commandList[index+1], functionsLList); 
             }
         }
-        checkSyntaxStructure(commandList, --index, functionsLList);
+        if (checkRes.error && !checkRes.action) {
+            logError("Syntax Error", checkRes.error);
+        }
+        if (checkRes.action && checkRes.value && !checkRes.error) {
+            switch (checkRes.action) {
+                case "defun" : lastFuncLList.push(checkRes.value); break;
+                default : null;
+            }
+        }
+        checkSyntaxStructure(commandList, --index, lastFuncLList);
     }
-    return index;
+    return lastFuncLList;
 }
 
 export const initSA = (commandList) => {
-    checkSyntaxStructure(commandList, commandList.length-1, []);
+    logDebug("SA output", checkSyntaxStructure(commandList, commandList.length-1, []));
     return commandList;
 }
