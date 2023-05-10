ac180db6d99a63efa5bfa123fb893ed5162ea6ff
Saswata Das <dassaswata1996@gmail.com>
2023-05-10 19:13:23 +0530

    [WIP] check write keyword

--- src/syntax-analysis.js
+++ src/syntax-analysis.js
@@ -2,12 +2,33 @@
     Developing the command execution tree
 */
 
-import { logDebug, logError, logOutput } from './utils/logger.js'
+import { logDebug, logError, logOutput } from './utils/logger.js';
 
 const inKeywordsLChecklist = (keyword) => ["defun", "write", "terpri"].includes(keyword);
 
 const checkLDefun = (funcName, functionsLList) => (typeof funcName !== "string" || !isNaN(funcName) || inKeywordsLChecklist(funcName) || functionsLList.includes(funcName)) ?
-    {action: null, value: null, error: `${funcName} cannot be a function name`} : {action: "defun", value: funcName, error: null};
+    {action: "defun", value: true, error: `${funcName} cannot be a function name`} : {action: "defun", value: funcName, error: false};
+
+const checkLSolvableOperation = (operation, index, funcLList) => {
+    const operatorLList = ["+", "-", "*", "/", "mod"];
+    if (index >= 0) {
+        if ((index === 0 && operatorLList.includes(operation[index]) || funcLList.concat["read"].includes(operation[index])) || (index !== 0 && !operatorLList.includes(operation[index]))) // TODO: complete check all possible conditions
+            checkLSolvableOperation(operation, --index);
+        else
+            return false;
+    }
+    return true;
+}
+
+const checkLWrite = (wOutput, funcLList) => {
+    if (typeof wOutput === "object" && checkLSolvableOperation(wOutput, wOutput.length-1, funcLList)) {
+        return {action: "write", value: true, error: false};
+    } else if (typeof wOutput === "string" || typeof wOutput === "number") {
+        return {action: "write", value: true, error: false};
+    } else {
+        return {action: "write", value: false, error: `cannot Write value of ${wOutput}`};
+    }
+};
 
 const checkSyntaxStructure = (commandList, index, functionsLList) => {
     let lastFuncLList = functionsLList;
@@ -16,16 +37,17 @@ const checkSyntaxStructure = (commandList, index, functionsLList) => {
         if (typeof commandList[index] === "object") {
             checkSyntaxStructure(commandList[index], commandList[index].length-1, lastFuncLList);
         } else {
-            if (commandList[index] === "defun") {
-                checkRes = checkLDefun(commandList[index+1], functionsLList); 
-            }
+            if (commandList[index] === "defun")
+                checkRes = checkLDefun(commandList[index+1], functionsLList);
+            if (commandList[index] === "write")
+                checkRes = checkLWrite(commandList[index+1], functionsLList);
         }
-        if (checkRes.error && !checkRes.action) {
+        if (checkRes.error) {
             logError("Syntax Error", checkRes.error);
         }
-        if (checkRes.action && checkRes.value && !checkRes.error) {
+        if (checkRes.value) {
             switch (checkRes.action) {
-                case "defun" : lastFuncLList.push(checkRes.value); break;
+                case "defun" : lastFuncLList.push(Symbol(checkRes.value)); break;
                 default : null;
             }
         }
