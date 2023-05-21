b48311d875c85f0fda885fd0bbe9a761bdb77d21
Saswata Das <dassaswata1996@gmail.com>
2023-05-21 23:11:12 +0530

    convert string to numbers

--- src/lexical-conversion.js
+++ src/lexical-conversion.js
@@ -2,7 +2,7 @@
     Developing command tree
 */
 
-import { logDebug, logOutput } from './utils/logger.js';
+import { logDebug, logError, logOutput } from './utils/logger.js';
 import { initSAOperationCheck, initSAScopeCheck } from './syntax-analysis.js';
 
 class NodeDef {
@@ -12,7 +12,7 @@ class NodeDef {
         this.listEndIndex = -1;
         this.children = [];
         this.expression = [];
-        this.output = '';
+        this.output = null;
     }
 
     getNodeDetails = () => ({
@@ -33,13 +33,15 @@ class NodeDef {
     updateId = (id) => this.id = id;
 }
 
-const isOperand = (char, charCount = 0, check = true) => {
+const checkIsNumber = (char, lexCheck = false, charCount = 0, check = true) => {
+    if (char === "-" && !lexCheck)
+        return false;
     if (charCount < char.length) {
         if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "."].includes(char[charCount]))
             check = check && true;
         else
             check = check && false;
-        isOperand(char, ++charCount, check);
+        checkIsNumber(char, lexCheck, ++charCount, check);
     }
     return check;
 }
@@ -61,6 +63,40 @@ const isWhitespace = (char) => {
         || char === '\u205f'
         || char === '\u3000'
         || char === '\ufeff'
+}
+
+const charToNumberMap = (char) => {
+    switch (char) {
+        case "0" : return 0;
+        case "1" : return 1;
+        case "2" : return 2;
+        case "3" : return 3;
+        case "4" : return 4;
+        case "5" : return 5;
+        case "6" : return 6;
+        case "7" : return 7;
+        case "8" : return 8;
+        case "9" : return 9;
+        case "-" : return -1;
+        case "." : return 0.1;
+        default : logError('Bad Compiler Bad Dev!!!!', 'String is not number'); return 0;
+    }
+}
+
+const convertStringToNumber = (sNum, index = 0, output = 0, unit = 1, ngtv = 1, dec = 1) => {
+    if (index < sNum.length) {
+        const numVal = charToNumberMap(sNum[index]);
+        if (numVal !== -1 && numVal !== 0.1) {
+            output = numVal + output*unit;
+            unit = unit * 10
+        }
+        if (numVal === -1)
+            ngtv = -1;
+        if (numVal === 0.1)
+            dec = dec * numVal;
+        return convertStringToNumber(sNum, ++index, output, unit, ngtv, dec);
+    }
+    return output * ngtv * dec;
 }
 
 const convertLispToScopeMap = (lispChars, index, charList, parenthesisScopeMapTemp, parenthesisScopeMap, spaceCount) => {
@@ -91,7 +127,7 @@ const convertLispToScopeMap = (lispChars, index, charList, parenthesisScopeMapTe
                 if (parenthesisScopeMapTemp.length > 0)
                     parenthesisScopeMapTemp[parenthesisScopeMapTemp.length-1].updateChild(parenthesisScopeMap[parenthesisScopeMap.length-1].id);
             }
-            charList.push(lispChars[index]);
+            charList.push(checkIsNumber(lispChars[index]) ? convertStringToNumber(lispChars[index]) : lispChars[index]);
         } else {
             ++spaceCount;
         }
@@ -131,7 +167,7 @@ const clearSourceMapJunks = (scopeMap, index) => {
 
 const detectOperands = (lispLines, index, lispChars) => {
     if (index < lispLines.length) {
-        if (index-1 > 0 && isOperand(lispLines[index]) && isOperand(lispChars[lispChars.length-1])) {
+        if (index-1 > 0 && checkIsNumber(lispLines[index]) && checkIsNumber(lispChars[lispChars.length-1], true)) {
             lispChars[lispChars.length-1] = lispChars[lispChars.length-1]+lispLines[index];
         } else {
             lispChars.push(lispLines[index]);
--- src/solve-tree.js
+++ src/solve-tree.js
@@ -2,6 +2,14 @@
     Solve code
 */
 
+const solveExpression = (operator, expression, index, output = 0) => {
+    if (index < expression.length) {
+        if (operator === "+") output = output + expression[index];
+        return solveExpression(operator, expression, index, output);
+    }
+    return output;
+}
+
 const interpretExpression = (abstractTree, index) => {
     if (index < abstractTree.length) {
         return interpretExpression(abstractTree, ++index);
