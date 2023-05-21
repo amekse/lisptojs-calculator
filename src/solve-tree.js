c2840595db5981fd55067e53e2072eaf8c979c9a
Saswata Das <dassaswata1996@gmail.com>
2023-05-21 23:35:55 +0530

    [WIP] fix tree results print

--- src/solve-tree.js
+++ src/solve-tree.js
@@ -2,30 +2,33 @@
     Solve code
 */
 
-const solveExpression = (expression, operator = expression[0], index = 2, output = expression[1]) => {
-    // TODO: check output if object
+import { logDebug } from './utils/logger.js';
+
+const solveExpression = (absTree, expression, operator = expression[0], index = 2, output = (typeof expression[1] === "object" ? absTree[expression[1][0]].output : expression[index])) => {
     if (index < expression.length) {
-        if () {
-            
-        }
-        if (operator === "+") output = output + expression[index];
-        if (operator === "-") output = output - expression[index];
-        if (operator === "/") output = output / expression[index];
-        if (operator === "*") output = output * expression[index];
+        if (operator === "+") output = output + (typeof expression[index] === "object" ? absTree[expression[index][0]].output : expression[index]);
+        if (operator === "-") output = output - (typeof expression[index] === "object" ? absTree[expression[index][0]].output : expression[index]);
+        if (operator === "/") output = output / (typeof expression[index] === "object" ? absTree[expression[index][0]].output : expression[index]);
+        if (operator === "*") output = output * (typeof expression[index] === "object" ? absTree[expression[index][0]].output : expression[index]);
         return solveExpression(expression, operator, ++index, output);
     }
-    return output;
+    if (index === expression.length-1) {
+        absTree.putOutput(output);
+    }
+    return absTree;
 }
 
-const interpretExpression = (abstractTree, index) => {
-    if (index < abstractTree.length) {
-        abstractTree[index].putOutput(solveExpression(abstractTree[index].expression));
-        return interpretExpression(abstractTree, ++index);
-    }
-    return abstractTree[index];
+const interpretExpression = (abstractTree, index = 0) => {
+    if (index < abstractTree.length)
+        return interpretExpression(solveExpression(abstractTree, abstractTree[index].expression), ++index);
+    return abstractTree;
 }
 
 export const initST = (abstractTree) => {
-    interpretExpression(abstractTree, 0);
+    logDebug("**ST Output**")
+    logDebug(JSON.stringify(interpretExpression(abstractTree)))
+    // interpretExpression(abstractTree, 0).map(i => {
+    //     logDebug(JSON.stringify(i));
+    // });
     return true;
 }
